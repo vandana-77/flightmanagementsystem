96bab3ce9221e75e1ace512b60be580913740680
BVRIT <BVRIT@PLACEMENT-LAB-17>
2020-02-22 14:15:47 +0530

    project

---
+++ Flight_Reservation/src/com/capgemini/corejava/flightservice/FlightService.java
@@ -0,0 +1,9 @@
+package com.capgemini.corejava.flightservice;
+
+public interface FlightService {
+	
+	public static int addFlightService(int flightNumber,String flightModel,String carrierName,int seatCapacity) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
